0cb66c2fca320eb78896e33530a930809e629418
RPV <rpv@senai.br>
2026-02-24 22:12:17 -0300

    cadastro1.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Listagem } from './pages/Listagem'
-<<<<<<< HEAD
-import { Cadastrar } from './pages/Cadastrar'
-import Agendar from './pages/Agendar'
-=======
 import { Consultas } from './pages/Consultas'
 import { Pets } from './pages/Pets'
->>>>>>> 09f4e8c31bb82e5b5ba99231bd74dcf9fdf9015a
 export function App() {
 
     return (
